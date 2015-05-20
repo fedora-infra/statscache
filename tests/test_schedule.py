b97bd0fa60e5c2373a001087f1421f32fbc4d8a8
Ralph Bean <rbean@redhat.com>
2015-05-20 10:13:26 -0400

    Fix the tests (they didn't account for the epsilon fix).

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -13,22 +13,37 @@ class TestSchedule(unittest.TestCase):
     @freezegun.freeze_time('2012-01-14 00:00:00')
     def test_basic(self):
         s = Schedule(minute=[15], hour=[5])
-        self.assertEquals(float(s), 5 * 60 * 60 + 15 * 60)
+        self.assertEquals(float(s), 5 * 60 * 60 + 15 * 60 + 1)
+
+    @freezegun.freeze_time('2012-01-14 00:00:00')
+    def test_one_second(self):
+        s = Schedule()
+        self.assertEquals(float(s), 1)
+
+    @freezegun.freeze_time('2012-01-14 00:00:01')
+    def test_one_second_ahead(self):
+        s = Schedule()
+        self.assertEquals(float(s), 1)
+
+    @freezegun.freeze_time('2012-01-14 00:00:00')
+    def test_two_seconds(self):
+        s = Schedule(second=[2])
+        self.assertEquals(float(s), 2)
 
     @freezegun.freeze_time('2012-01-14 12:00:00')
     def test_wrap_day(self):
         s = Schedule(minute=[15], hour=[5])
-        self.assertEquals(float(s), 17 * 60 * 60 + 15 * 60)
+        self.assertEquals(float(s), 17 * 60 * 60 + 15 * 60 + 1)
 
     @freezegun.freeze_time('2012-01-14 12:15:00')
     def test_wrap_day_match_minute(self):
         s = Schedule(minute=[15], hour=[5])
-        self.assertEquals(float(s), 17 * 60 * 60)
+        self.assertEquals(float(s), 17 * 60 * 60 + 1)
 
     @freezegun.freeze_time('2012-01-14 12:16:00')
     def test_wrap_day_wrap_minute(self):
         s = Schedule(minute=[15], hour=[5])
-        self.assertEquals(float(s), 16 * 60 * 60 + 59 * 60)
+        self.assertEquals(float(s), 16 * 60 * 60 + 59 * 60 + 1)
 
     @freezegun.freeze_time('2012-01-14 00:00:00')
     def test_working_with_time_sleep(self):
